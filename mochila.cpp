3ce2295c553ae4263ec6234f69c16def47574608
Alba-Sangil <alba.sangil@gmail.com>
2023-11-08 12:36:20 +0100

    mochila

--- mochila.cpp
+++ mochila.cpp
@@ -1,38 +1,61 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
 
 using namespace std;
 
-// Estructura para representar un objeto con peso y beneficio
-struct Objeto {
-    int peso;
-    int beneficio;
-};
-
 // Función para resolver el problema de la mochila
-int mochila(int num_objetos, vector<Objeto> objetos, int peso_maximo) {
-    vector<vector<int>> dp(num_objetos + 1, vector<int>(peso_maximo + 1, 0));
+int mochila(int num_objetos, int pesos[], int beneficios[], int peso_maximo) {
+    int dp[num_objetos + 1][peso_maximo + 1] = {0};
+    int decision[num_objetos + 1][peso_maximo + 1] = {0};
 
     for (int i = 1; i <= num_objetos; i++) {
         for (int j = 0; j <= peso_maximo; j++) {
-            if (objetos[i - 1].peso <= j) {
-                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - objetos[i - 1].peso] + objetos[i - 1].beneficio);
+            if (pesos[i - 1] <= j) {
+                int sin_objeto = dp[i - 1][j];
+                int con_objeto = dp[i - 1][j - pesos[i - 1]] + beneficios[i - 1];
+
+                if (con_objeto > sin_objeto) {
+                    dp[i][j] = con_objeto;
+                    decision[i][j] = 1;  // Tomar el objeto
+                } else {
+                    dp[i][j] = sin_objeto;
+                    decision[i][j] = 0;  // Descartar el objeto
+                }
             } else {
                 dp[i][j] = dp[i - 1][j];
+                decision[i][j] = 0;  // Descartar el objeto
             }
         }
     }
+
+    // Reconstruir la solución (array de decisiones)
+    int solucion[num_objetos];
+    int i = num_objetos, j = peso_maximo;
+    for (int k = num_objetos - 1; k >= 0; k--) {
+        solucion[k] = decision[i][j];
+        if (decision[i][j] == 1) {
+            j -= pesos[i - 1];
+        }
+        i--;
+    }
+
+    // Imprimir la solución
+    cout << "Solución: ";
+    for (int k = 0; k < num_objetos; k++) {
+        cout << solucion[k] << " ";
+    }
+    cout << endl;
 
     return dp[num_objetos][peso_maximo];
 }
 
 int main() {
     int num_objetos = 3;
-    vector<Objeto> objetos = {{9, 38}, {6, 40}, {5, 24}};
+    int pesos[] = {9, 6, 5};
+    int beneficios[] = {38, 40, 24};
     int peso_maximo = 15;
 
-    int max_beneficio = mochila(num_objetos, objetos, peso_maximo);
+    int max_beneficio = mochila(num_objetos, pesos, beneficios, peso_maximo);
     cout << "Máximo beneficio: " << max_beneficio << endl;
 
     return 0;
