61440fc64bb516152ff3b3f93369b9f806b58a3a
Alba-Sangil <alba.sangil@gmail.com>
2023-11-08 18:43:55 +0100

    Mochila

--- mochila.cpp
+++ mochila.cpp
@@ -2,58 +2,54 @@
 #include<algorithm>
 using namespace std;
 
-int mochila(int b[], int p[], int N, int M,int x[N]){
-	int** matriz = new int*[N];
-	for(int i = 0; i < N; i++){
-		x[i] = 0;
-	}
+
+int mochila(int N, int *p ,int *b , int M){
+	int** matrix = new int*[N];	
+
 	for(int i=0; i<=N; i++){
-		matriz[i] = new int[M + 1];
+		matrix[i] = new int[M + 1];
 		for(int j = 0; j<=M; j++){
-			matriz[i][j] = 0;
+			matrix[i][j] = 0;
 		}
 	}
+
 	for(int i=0; i<=N; i++){
-		for(int j = 0; j<=M; j++){
+                 for(int j = 0; j<=M; j++){
 			if( i == 0){
-				matriz[i][j] = 0;
+				matrix[i][j] = 0;
 			}
 			else if(j == 0){
-				matriz[i][j] = 0;
+				matrix[i][j] = 0;
 			}
 			else if (i == 1 && p[i] < j){
-				matriz[i][j] = b[i];
-				x[i] = 1;
+				matrix[i][j] = b[i];
 			}
 					
 			else if( p[i] > j){
-			 	matriz[i][j] = matriz[i - 1][j];
+			 	matrix[i][j] = matrix[i - 1][j];
 			} 
 		   
 			else {
-				if (max(matriz[i - 1][j], matriz[i - 1][j - p[i]] + b[i])==matriz[i - 1][j - p[i]] + b[i]){
-					x[i] = 1;
-				}
-				matriz[i][j] = max(matriz[i - 1][j], matriz[i - 1][j - p[i]] + b[i]);
+				matrix[i][j] = std::max(matrix[i - 1][j], matrix[i - 1][j - p[i]] + b[i]);
 	
 			} 
 		}
 	}
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < M; j++) {
-			cout << matriz[i][j] << " ";
+			cout << matrix[i][j] << " ";
 		}
 		cout << endl;
 	}
-	return 0;
+
+	return matrix[N-1][M];
 }
 int main(){
-    int b[] = { 38, 40, 24 };
-    int p[] = { 9, 6, 5 };
+    int b[4] = {0, 38, 40, 24 };
+    int p[4] = {0, 9, 6, 5 };
     int M = 15;
     int N = 3;
-    int x[N]
-    int valor_mochila = mochila(p, b,N , M,x);
+    int valor_mochila = mochila(N, p, b, M);
     cout << "El valor de la mochila es: " << valor_mochila <<endl;
     return 0;
 }
